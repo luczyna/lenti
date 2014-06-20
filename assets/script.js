ca0c20e10846751f4ac669bc35dd083a7a6710cc
Ana Lu <luczynaki@gmail.com>
2014-06-20 12:27:02 -0500

    finish setting up new game environment

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+/* this will be a collection
+ * of minimized script files
+ * for production use
+ *
+ * list is as follows:
+ * lenti.js
+ * items.js
+ * clans.js
+ * monsters.js
+ * treasure.js
+ * achievements.js
+ *
+ */
